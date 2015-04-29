3df6b758cbe01ed2a416c9a030d15819f16733fc
storm <tabooc@163.com>
2015-04-29 14:00:22 +0800

    修仙之路

---
+++ js/tile.js
@@ -0,0 +1,31 @@
+/*
+  增加text属性
+*/
+function Tile(position, value, text) {
+  this.x                = position.x;
+  this.y                = position.y;
+  this.value            = value || 2;
+  this.text             = text || '旋照';
+  this.previousPosition = null;
+  this.mergedFrom       = null; // Tracks tiles that merged together
+}
+
+Tile.prototype.savePosition = function () {
+  this.previousPosition = { x: this.x, y: this.y };
+};
+
+Tile.prototype.updatePosition = function (position) {
+  this.x = position.x;
+  this.y = position.y;
+};
+
+Tile.prototype.serialize = function () {
+  return {
+    position: {
+      x: this.x,
+      y: this.y
+    },
+    value: this.value,
+    text: this.text
+  };
+};
